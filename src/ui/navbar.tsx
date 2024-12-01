573796b9657448117d737731ef884e812f6e0099
arashveysi3 <am7team@gmail.com>
2024-12-02 01:13:13 +0330

    Navbar designed
    initial design

--- src/ui/navbar.tsx
+++ src/ui/navbar.tsx
@@ -1,25 +1,20 @@
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu";
-
+import { Button } from "@/components/ui/button";
+import { Menu, Sun } from "lucide-react";
 export default function Navbar() {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <div className="w-full h-12 flex flex-row-reverse my-2 pb-8 gap-2 items-center border-b border-gray-100 backdrop-blur-sm">
+      <Button variant={"outline"} className="mr-4 px-3">
+        <Menu />
+      </Button>
+      <Button
+        variant={"outline"}
+        className="mr-1 items-center justify-center px-3"
+      >
+        <Sun />
+      </Button>
+      <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold items-start mr-auto ml-2">
+        @snippetBase/react-components-hub
+      </code>
+    </div>
   );
 }
