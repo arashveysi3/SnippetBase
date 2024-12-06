d1329676ebab7b87b069aae6a279af20ce387176
arashveysi3 <am7team@gmail.com>
2024-12-06 23:27:28 +0330

    main page predesign done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
 export default function Home() {
   return (
     <div>
-      <main></main>
+      <main>
+        <div className="grid sm:grid-cols-3 grid-cols-2 gap-4 w-full justify-items-center ml-4">
+          <div className="justify-self-end">
+            <div className="w-[24rem] h-[22rem] bg-gray-200 rounded-md my-4"></div>
+            <div className="w-[24rem] h-[22rem] bg-gray-200 rounded-md my-4"></div>
+            <div className="w-[24rem] h-[22rem] bg-gray-200 rounded-md my-4"></div>
+            <div className="w-[24rem] h-[22rem] bg-gray-200 rounded-md my-4"></div>
+            <div className="w-[24rem] h-[22rem] bg-gray-200 rounded-md my-4"></div>
+            <div className="w-[24rem] h-[22rem] bg-gray-200 rounded-md my-4"></div>
+          </div>
+          <div className="justify-self-start">
+            <div className="w-[24rem] h-[22rem] bg-gray-200 rounded-md my-4"></div>
+            <div className="w-[24rem] h-[22rem] bg-gray-200 rounded-md my-4"></div>
+            <div className="w-[24rem] h-[22rem] bg-gray-200 rounded-md my-4"></div>
+            <div className="w-[24rem] h-[22rem] bg-gray-200 rounded-md my-4"></div>
+          </div>
+          <div className="justify-self-center">
+            <div className="w-[16rem] h-[65rem] bg-gray-200 rounded-md my-4"></div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
--- src/ui/navbar.tsx
+++ src/ui/navbar.tsx
@@ -4,7 +4,7 @@ import { Menu } from "lucide-react";
 export default function Navbar() {
   return (
     <div
-      className="w-full h-12 flex flex-row-reverse mb-2 pb-1 pt-2 gap-2 items-center border-b border-gray-100 backdrop-blur-sm bg-gray-100 dark:bg-gray-900
+      className="w-full h-14 flex flex-row-reverse mb-2 py-1 gap-2 items-center border-b border-gray-100 backdrop-blur-sm bg-gray-100 dark:bg-gray-900
 "
     >
       <Button
@@ -14,7 +14,7 @@ export default function Navbar() {
         <Menu />
       </Button>
       <ThemeToggleButton />
-      <code className="relative rounded bg-muted dark:text-white dark:bg-gray-800 px-3 py-[0.2rem] font-mono text-sm font-semibold items-start mr-auto ml-2 hover:text-blue-500 transition-all cursor-pointer">
+      <code className="relative rounded bg-muted dark:text-white bg-gray-200 dark:bg-gray-800 px-3 py-[0.2rem] font-mono text-sm font-semibold items-start mr-auto ml-2 hover:text-blue-500 transition-all cursor-pointer">
         @snippetBase/react-components-hub
       </code>
     </div>
